ab9db57ca027c399d246a69c7e2ee2fd0658a85c
Arthur Olga <arthur_olga@hotmail.com>
2019-11-26 23:54:11 -0300

    config

---
+++ config.py
@@ -0,0 +1,10 @@
+ownerName = 'Arthur'
+
+keyName = 'virginia'
+keyName2 = 'ohio'
+
+securityGroupName = 'Arthur-SG'
+targetGroupName = 'Arthur-target-group'
+loadBalancerName = 'arthur-load-balancer'
+launch_config_name = 'arthur-launch-configuration'
+autoscaling_name = 'arthur-autoscaling-group'
